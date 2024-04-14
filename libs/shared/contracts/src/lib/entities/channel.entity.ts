02a97fb444a9df4a3e647c9dda31c6af1131ece3
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-04-14 11:21:30 +0200

    implement: channel entity and channel type enum

---
+++ libs/shared/contracts/src/lib/entities/channel.entity.ts
@@ -0,0 +1,10 @@
+import { ChannelType } from "../enums/channel-type"
+
+export interface ChannelEntity {
+    id: string
+    name: string
+    server_id: string | null
+    type: ChannelType
+    created_at: string
+    updated_at: string
+}
