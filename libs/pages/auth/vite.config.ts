86daa91c7a6157e75c0e42c61aa19a2dc127e027
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2024-04-29 23:08:09 +0200

    fix(): remove build in vite.config.ts for all libs

--- libs/pages/auth/vite.config.ts
+++ libs/pages/auth/vite.config.ts
@@ -25,27 +25,6 @@ export default defineConfig({
 
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
-  build: {
-    outDir: '../../../dist/libs/pages/auth',
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-    lib: {
-      // Could also be a dictionary or array of multiple entry points.
-      entry: 'src/index.ts',
-      name: 'auth',
-      fileName: 'index',
-      // Change this to the formats you want to support.
-      // Don't forget to update your package.json as well.
-      formats: ['es', 'cjs'],
-    },
-    rollupOptions: {
-      // External packages that should not be bundled into your library.
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
-    },
-  },
-
   test: {
     globals: true,
     cache: {
