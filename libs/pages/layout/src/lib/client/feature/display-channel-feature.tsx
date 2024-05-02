2d86edf6406c3681ffaa7c5c826da981c84695f9
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2024-05-02 23:22:26 +0200

    enter channel on click

--- libs/pages/layout/src/lib/client/feature/display-channel-feature.tsx
+++ libs/pages/layout/src/lib/client/feature/display-channel-feature.tsx
@@ -1,22 +1,24 @@
 import { ChannelEntity } from '@beep/contracts'
 import DisplayChannel from '../ui/display-channel'
 import { toast } from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
 
 interface DisplayChannelFeature {
   channel: ChannelEntity
 }
 
-const onJoinChannel = () => {
-  console.log('Join channel')
+const onDeleteChannel = () => {
+  console.log('Delete channel')
+  toast.success('Channel deleted !')
 }
 
-export default function DisplayChannelFeature({
-  channel,
-}: DisplayChannelFeature) {
-  const onDeleteChannel = () => {
-    console.log('Delete channel')
-    toast.success('Channel deleted !')
+export default function DisplayChannelFeature({ channel }: DisplayChannelFeature) {
+  const navigate = useNavigate()
+
+  const onJoinChannel = (channelId: string) => {
+    navigate(`/channels/${channelId}`)
   }
+
   return (
     <DisplayChannel
       channel={channel}
