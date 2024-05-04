97cfb3a8a0580077e5e834b474615e9693fa876c
Dorian TETU <dorian.tetu@etu.umontpellier.fr>
2024-05-04 23:51:07 +0200

    feat(server): add server API and endpoints

---
+++ libs/domains/server/src/lib/server.api.tsx
@@ -0,0 +1,39 @@
+import { backendUrl, ServerEntity } from '@beep/contracts';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { RootState } from '@beep/store';
+import { join } from 'path';
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: backendUrl,
+    credentials: 'include',
+    prepareHeaders: (headers, { getState }) => {
+      const { user } = getState() as RootState;
+      headers.set('Authorization', `Bearer ${user.tokens.accessToken}`);
+    },
+});
+  
+export const serverApi = createApi({
+    reducerPath: 'serverApi',
+    baseQuery,
+    tagTypes: ['servers'],
+    endpoints: (builder) => ({
+      getServers: builder.query<ServerEntity[], void>({
+        query: (params) => `/server`,
+        providesTags: ['servers']
+      }),
+      joinServer: builder.mutation<void, string>({
+        query: (serverId) => ({
+          url: `/server/${serverId}`,
+          method: 'POST',
+        }),
+        invalidatesTags: ['servers'],
+      }),
+    })
+  })
+  
+export const {
+  useGetServersQuery,
+  useJoinServerMutation,
+  } = serverApi
+  
+  
