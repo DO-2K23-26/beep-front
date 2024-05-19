df59010727477ae858ec362e7c38714495caf870
benoit-planche <benoit.planche@etu.umontpellier.fr>
2024-05-19 18:13:03 +0200

    feat: Add ConnectedUserRow component
    The ConnectedUserRow component is added to the client UI in order to display a row for each connected user. This component includes the user's name and an avatar image.

---
+++ libs/pages/layout/src/lib/client/ui/connected-user-row.tsx
@@ -0,0 +1,25 @@
+interface ConnectedUserRowProps {
+    name: string
+  }
+  
+  export default function ConnectedUserRow({ name }: ConnectedUserRowProps) {
+    return (
+      <div className="flex flex-row justify-between items-center p-2 hover:bg-violet-400 rounded-xl transition-all cursor-pointer w-full group gap-2">
+        <div className="flex flex-row gap-3 items-center">
+          <img
+            className="w-9 min-w-[36px] h-9 min-h-[36px] bg-violet-50 rounded-xl"
+            src={'image'}
+            alt={'img'}
+          />
+          <h5 className="font-semibold text-xs truncate w-[75px] sm:w-[125px]">
+            {name}
+          </h5>
+        </div>
+        <div className="flex-row gap-3 items-center flex">
+          <button className="hidden sm:flex invisible group-hover:visible"></button>
+          <span className="flex flex-row gap-2"></span>
+        </div>
+      </div>
+    )
+  }
+  
