b5d8d23ffa09cfedb26765766575697ba952d824
benoit-planche <benoit.planche@etu.umontpellier.fr>
2024-05-19 18:19:44 +0200

    feat: Add ListConnectedUsers component
    The ListConnectedUsers component is added to the client UI to display a list of connected users. This component takes an array of UserConnectedEntity objects as input and renders a ConnectedUserRow component for each user, showing their username. This change improves the user interface by providing a visual representation of the connected users.

---
+++ libs/pages/layout/src/lib/client/ui/list-connected-user.tsx
@@ -0,0 +1,16 @@
+import { UserConnectedEntity } from '@beep/contracts'
+import ConnectedUserRow from './connected-user-row'
+
+export interface ListConnectedUsersProps {
+  users: UserConnectedEntity[]
+}
+
+export function ListConnectedUsers({ users }:ListConnectedUsersProps) {
+  return (
+    <>
+      {users.map((user) => (
+        <ConnectedUserRow key={user.id} name={user.username} />
+      ))}
+    </>
+  )
+}
