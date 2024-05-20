7489bf4248ee22ed7dcea4cc3c65886d5c3e97a4
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2024-05-20 18:01:48 +0200

    Add a default profile picture of a connected user row

--- libs/pages/layout/src/lib/client/ui/connected-user-row.tsx
+++ libs/pages/layout/src/lib/client/ui/connected-user-row.tsx
@@ -1,14 +1,15 @@
 interface ConnectedUserRowProps {
     name: string
+    profilePicture?: string
   }
   
-  export default function ConnectedUserRow({ name }: ConnectedUserRowProps) {
+  export default function ConnectedUserRow({ name, profilePicture }: ConnectedUserRowProps) {
     return (
       <div className="flex flex-row justify-between items-center p-2 hover:bg-violet-400 rounded-xl transition-all cursor-pointer w-full group gap-2">
         <div className="flex flex-row gap-3 items-center">
           <img
             className="w-9 min-w-[36px] h-9 min-h-[36px] bg-violet-50 rounded-xl"
-            src={'image'}
+            src={profilePicture || '/picture.svg'}
             alt={'img'}
           />
           <h5 className="font-semibold text-xs truncate w-[75px] sm:w-[125px]">
