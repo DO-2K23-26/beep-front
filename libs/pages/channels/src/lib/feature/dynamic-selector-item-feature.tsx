78d6851e1eb2f81150c5e0cca49aca546be85803
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2024-06-26 17:59:46 +0200

    remove unused imports

--- libs/pages/channels/src/lib/feature/dynamic-selector-item-feature.tsx
+++ libs/pages/channels/src/lib/feature/dynamic-selector-item-feature.tsx
@@ -15,7 +15,7 @@ export const DynamicSelectorFeature = ({ user }: DynamicSelectorProps) => {
           <img
             className="w-9 min-w-[36px] h-9 min-h-[36px] object-cover bg-violet-50 rounded-xl"
             src={userProfilePicture || '/picture.svg'}
-            alt={user.username + '-img'}
+            alt={user.username + ' profile picture'}
           />
           <p>{user.username}</p>
         </div>
