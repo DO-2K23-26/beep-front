f50891a32edbbc2644a7477d2f3a59fc73c6719c
hugoPonthieu <hugo.ponthieu@etu.umontpellier.fr>
2024-11-05 00:33:32 +0100

    poc(embed):list

--- libs/pages/channels/src/lib/feature/media-embed-feature.tsx
+++ libs/pages/channels/src/lib/feature/media-embed-feature.tsx
@@ -1,12 +1,12 @@
-type MediaType =
-  | 'youtube'
-  | 'spotify'
-  | 'appleMusic'
-  | 'instagram'
-  | 'tiktok'
-  | 'twitter'
-  | 'else'
-
+enum MediaType {
+  youtube = 'youtube',
+  spotify = 'spotify',
+  appleMusic = 'appleMusic',
+  instagram = 'instagram',
+  tiktok = 'tiktok',
+  twitter = 'twitter',
+  else = 'else'
+}
 interface Media {
   type: MediaType
   id: string
@@ -27,28 +27,24 @@ const appleMusicRegex =
   /(?:https?:\/\/)?(?:music\.apple\.com\/(?:[a-z]{2}\/)?(?:album|playlist|track)\/[\w\\-]+\/)(\d+)/
 
 export const detectMedia = (text: string): Media | null => {
-  const youtubeMatch = text.match(youtubeRegex)
-  const spotifyMatch = text.match(spotifyRegex)
-  const appleMusicMatch = text.match(appleMusicRegex)
-  const instagramMatch = text.match(instagramRegex)
-  const tiktokMatch = text.match(tiktokRegex)
-  const tiktokShortMatch = text.match(tiktokShortRegex)
-  const twitterMatch = text.match(twitterRegex)
-
-  if (youtubeMatch) {
-    return { type: 'youtube', id: youtubeMatch[1] }
-  } else if (spotifyMatch) {
-    return { type: 'spotify', id: spotifyMatch[1] }
-  } else if (appleMusicMatch) {
-    return { type: 'appleMusic', id: appleMusicMatch[1] }
-  } else if (instagramMatch) {
-    return { type: 'instagram', id: instagramMatch[1] }
-  } else if (tiktokMatch) {
-    return { type: 'tiktok', id: tiktokMatch[1] }
-  } else if (tiktokShortMatch) {
-    return { type: 'tiktok', id: tiktokShortMatch[1] }
-  } else if (twitterMatch) {
-    return { type: 'twitter', id: twitterMatch[2] }
+  // Maybe each media should be a list of { regex: regex, type: MediaType }
+  const regexMap: { [key: string]: { regex: RegExp; type: MediaType } } = {
+    youtube: { regex: youtubeRegex, type: MediaType.youtube },
+    spotify: { regex: spotifyRegex, type: MediaType.spotify },
+    appleMusic: { regex: appleMusicRegex, type: MediaType.appleMusic },
+    instagram: { regex: instagramRegex, type: MediaType.instagram },
+    tiktok: { regex: tiktokRegex, type: MediaType.tiktok },
+    tiktokShort: { regex: tiktokShortRegex, type: MediaType.tiktok },
+    twitter: { regex: twitterRegex, type: MediaType.twitter },
   }
-  return { type: 'else', id: text }
+
+  // You could easily return a list of media
+  for (const key in regexMap) {
+    const match = text.match(regexMap[key].regex)
+    if (match) {
+      return { type: regexMap[key].type, id: match[1] }
+    }
+  }
+
+  return { type: MediaType.else, id: text }
 }
