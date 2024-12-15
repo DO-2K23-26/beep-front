6ee0c1f0b0688565acf9771537eae4e6ccd6cf3e
pauline <pauline.contat01@etu.umontpellier.fr>
2024-12-15 15:02:46 +0100

    fix bug in user media settings

--- libs/domains/voice/src/lib/voice.slice.ts
+++ libs/domains/voice/src/lib/voice.slice.ts
@@ -1,7 +1,12 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 // eslint-disable-next-line @nx/enforce-module-boundaries
-import { RootState } from '@beep/store';
-import { IVoice, Media, OccupiedChannelEntity, UserConnectedEntity } from '@beep/contracts'
+import { RootState } from '@beep/store'
+import {
+  IVoice,
+  Media,
+  OccupiedChannelEntity,
+  UserConnectedEntity,
+} from '@beep/contracts'
 
 export const initializeDevices = createAsyncThunk(
   'webRTC/initializeDevices',
@@ -38,53 +43,118 @@ const webrtcSlice = createSlice({
   name: 'webRTC',
   initialState: initialState,
   reducers: {
-
     setLocalStream(state, action: PayloadAction<MediaStream | null>) {
-      state.localStream = action.payload;
+      state.localStream = action.payload
     },
     addRemoteStream(state, action: PayloadAction<RTCRtpTransceiver>) {
-      state.remoteStreams.push(action.payload);
+      state.remoteStreams.push(action.payload)
     },
     setRemoteStreams(state, action: PayloadAction<RTCRtpTransceiver[]>) {
       state.remoteStreams = action.payload
     },
     setConnectionState(state, action: PayloadAction<string>) {
-      state.connectionState = action.payload;
+      state.connectionState = action.payload
     },
     setChannelStatus(state, action: PayloadAction<string>) {
-      state.channelStatus = action.payload;
+      state.channelStatus = action.payload
     },
     setAudioInputDevice(state, action: PayloadAction<MediaDeviceInfo>) {
-      state.audioInputDevice = action.payload;
+      state.audioInputDevice = action.payload
     },
     setAudioOutputDevice(state, action: PayloadAction<Media>) {
-      state.audioOutputDevice = action.payload;
+      state.audioOutputDevice = action.payload
     },
     setVideoDevice(state, action: PayloadAction<MediaDeviceInfo>) {
-      state.videoDevice = action.payload;
+      state.videoDevice = action.payload
     },
     setDevices(state, action: PayloadAction<MediaDeviceInfo[]>) {
-      state.devices = action.payload;
+      state.devices = action.payload
+
+      if (state.audioInputDevice) {
+        const matchingAudioInput = action.payload.find(
+          (device) =>
+            device.deviceId === state.audioInputDevice.deviceId &&
+            device.kind === 'audioinput'
+        )
+
+        if (!matchingAudioInput) {
+          const firstAudioInput = action.payload.find(
+            (device) => device.kind === 'audioinput'
+          )
+          state.audioInputDevice = firstAudioInput || null
+        }
+      }
+
+      if (state.videoDevice) {
+        const matchingVideoInput = action.payload.find(
+          (device) =>
+            device.deviceId === state.videoDevice.deviceId &&
+            device.kind === 'videoinput'
+        )
+        if (!matchingVideoInput) {
+          const firstVideoInput = action.payload.find(
+            (device) => device.kind === 'videoinput'
+          )
+          state.videoDevice = firstVideoInput || null
+        }
+      }
+
+      if (state.audioOutputDevice) {
+        const matchingAudioOutput = action.payload.find(
+          (device) =>
+            device.deviceId === state.audioOutputDevice.id &&
+            device.kind === 'audiooutput'
+        )
+        if (!matchingAudioOutput) {
+          const firstAudioOutput = action.payload.find(
+            (device) => device.kind === 'audiooutput'
+          )
+          state.audioOutputDevice = firstAudioOutput
+            ? { id: firstAudioOutput.deviceId, name: firstAudioOutput.label }
+            : null
+        }
+      }
     },
     setCurrentChannelId(state, action: PayloadAction<string>) {
-      state.currentChannelId = action.payload;
+      state.currentChannelId = action.payload
     },
-    setSortedMembers(state, action: PayloadAction<{ user: UserConnectedEntity, stream: MediaStream }[]>) {
-      state.sortedMembers = action.payload;
-      console.log("update sortedMembers", state.sortedMembers)
+    setSortedMembers(
+      state,
+      action: PayloadAction<
+        { user: UserConnectedEntity; stream: MediaStream }[]
+      >
+    ) {
+      state.sortedMembers = action.payload
+      console.log('update sortedMembers', state.sortedMembers)
     },
-    setUserStreams(state, action: PayloadAction<{id: string, audio: string, video: string, channel: string }[]>) {
-      state.userStreams = action.payload;
+    setUserStreams(
+      state,
+      action: PayloadAction<
+        { id: string; audio: string; video: string; channel: string }[]
+      >
+    ) {
+      state.userStreams = action.payload
     },
     setServerPresence(state, action: PayloadAction<OccupiedChannelEntity>) {
-      if (!state.serverPresence.find((channel)=> action.payload.channelId === channel.channelId)){
-        state.serverPresence.push({channelId: action.payload.channelId, users: action.payload.users});
-      } else{
-        state.serverPresence = state.serverPresence.map((occupied) => occupied.channelId === action.payload.channelId ? action.payload : occupied);
+      if (
+        !state.serverPresence.find(
+          (channel) => action.payload.channelId === channel.channelId
+        )
+      ) {
+        state.serverPresence.push({
+          channelId: action.payload.channelId,
+          users: action.payload.users,
+        })
+      } else {
+        state.serverPresence = state.serverPresence.map((occupied) =>
+          occupied.channelId === action.payload.channelId
+            ? action.payload
+            : occupied
+        )
       }
-    }
+    },
   },
-});
+})
 
 export const {
   setLocalStream,
@@ -99,9 +169,9 @@ export const {
   setCurrentChannelId,
   setSortedMembers,
   setUserStreams,
-  setServerPresence
-} = webrtcSlice.actions;
+  setServerPresence,
+} = webrtcSlice.actions
 
-export const getVoiceState = (root: RootState) => root['webRTC'];
+export const getVoiceState = (root: RootState) => root['webRTC']
 
-export const webrtcSliceReducer = webrtcSlice.reducer;
+export const webrtcSliceReducer = webrtcSlice.reducer
