eb6c77cc856df80794de57b276b7e8695dca0425
pauline <pauline.contat01@etu.umontpellier.fr>
2024-12-22 13:58:19 +0100

    create notifications lib with nx

---
+++ libs/pages/notifications/src/lib/notifications.tsx
@@ -0,0 +1,11 @@
+import styles from './notifications.module.scss'
+
+export function Notifications() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Notifications!</h1>
+    </div>
+  )
+}
+
+export default Notifications
