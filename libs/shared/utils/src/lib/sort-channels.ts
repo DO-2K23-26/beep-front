00fc0fd5179725515342b26c55797165770190cf
tristan-mihai.radulescu <tristan-mihai.radulescu@etu.umontpellier.fr>
2024-12-24 12:44:18 +0100

    feat(channels): added position attribute and merged text channels with voice

---
+++ libs/shared/utils/src/lib/sort-channels.ts
@@ -0,0 +1,25 @@
+import { ChannelEntity, GetChannelsResponse } from '@beep/contracts'
+
+export function sortChannels(
+  channels: GetChannelsResponse | null | undefined
+): ChannelEntity[] {
+  const textChannels: ChannelEntity[] =
+    channels?.textChannels.map((i) => i) ?? []
+  const voiceChannels: ChannelEntity[] =
+    channels?.voiceChannels.map((i) => i) ?? []
+
+  let allChannels = [...textChannels, ...voiceChannels]
+
+  if (allChannels.length > 0) {
+    allChannels = allChannels.sort((a, b) => {
+      if (a.position < b.position) {
+        return -1
+      } else if (a.position > b.position) {
+        return 1
+      }
+
+      return a.id.localeCompare(b.id)
+    })
+  }
+  return allChannels
+}
