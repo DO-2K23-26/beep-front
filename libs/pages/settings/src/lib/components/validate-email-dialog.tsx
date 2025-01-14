16f750c317b67dcd2016fb47a2e309aa920f94f3
Giada De Martino <giada-de-martino@etu.umontpellier.fr>
2025-01-14 18:40:04 +0100

    translaction: validate email dialog

--- libs/pages/settings/src/lib/components/validate-email-dialog.tsx
+++ libs/pages/settings/src/lib/components/validate-email-dialog.tsx
@@ -18,11 +18,13 @@ export function ValidateEmailDialog({
 
   return (
     <DialogComponent
-      title={t('settings.modify-email-dialog.title')}
+      title={t('settings.components.validate-email-dialog.title')}
       triggerModalButton={
         !isModalOpen && ( // Hide the button when the modal is open
           <div>
-            <Button>Modify</Button>
+            <Button>
+              {t('settings.components.validate-email-dialog.modify')}
+            </Button>
           </div>
         )
       }
@@ -30,11 +32,17 @@ export function ValidateEmailDialog({
         <>
           <Controller
             name="password"
-            rules={{ required: 'Enter your current password' }}
+            rules={{
+              required: t(
+                'settings.components.validate-email-dialog.enter_password'
+              ),
+            }}
             control={updateEmailFormController.control}
             render={({ field, fieldState: { error } }) => (
               <InputText
-                label="Current Password"
+                label={t(
+                  'settings.components.validate-email-dialog.enter_password_label'
+                )}
                 type="password"
                 name="currentPassword"
                 className="w-full !rounded-lg min-h-[40px]"
@@ -47,16 +55,22 @@ export function ValidateEmailDialog({
           <Controller
             name="email"
             rules={{
-              required: 'Email is required',
+              required: t(
+                'settings.components.validate-email-dialog.enter_email'
+              ),
               pattern: {
                 value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: 'Invalid email address',
+                message: t(
+                  'settings.components.validate-email-dialog.enter_email_error'
+                ),
               },
             }}
             control={updateEmailFormController.control}
             render={({ field, fieldState: { error } }) => (
               <InputText
-                label="Email"
+                label={t(
+                  'settings.components.validate-email-dialog.enter_email_label'
+                )}
                 type="text"
                 name="email"
                 className="w-full !rounded-lg min-h-[40px]"
@@ -68,7 +82,9 @@ export function ValidateEmailDialog({
           />
         </>
       }
-      actionButtonTitle="Confirm"
+      actionButtonTitle={t(
+        'settings.components.validate-email-dialog.action_button'
+      )}
       action={action}
       isModalOpen={isModalOpen}
       setIsModalOpen={setIsModalOpen}
