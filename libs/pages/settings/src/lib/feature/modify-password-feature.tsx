f885e97e246b08560b47027a2ba2466ddc141ad0
Giada De Martino <giada-de-martino@etu.umontpellier.fr>
2025-01-16 10:17:33 +0100

    fix: variables cleanup of the file

--- libs/pages/settings/src/lib/feature/modify-password-feature.tsx
+++ libs/pages/settings/src/lib/feature/modify-password-feature.tsx
@@ -1,58 +1,51 @@
 import { UpdatePassword } from '@beep/contracts'
-import {
-  useChangePasswordMutation,
-  useUpdateMeMutation,
-} from '@beep/user'
+import { useChangePasswordMutation } from '@beep/user'
 import { useForm } from 'react-hook-form'
 import { useEffect, useState } from 'react'
 import { ModifyPasswordDialog } from '../components/modify-password-setting'
 import toast from 'react-hot-toast'
 
 export function ModifyPasswordFeature() {
-    const [isPasswordModalOpen, setIsPasswordModalOpen] = useState(false)
-    const [updateMe] = useUpdateMeMutation()
-    const [changePassword, result] = useChangePasswordMutation()
-    useEffect(() => {
-      if (result.isSuccess) {
-        toast.success('Update successful')
-      } else if (result.isError) {
-        toast.error('Something went wrong when updating your profile')
-      }
-    }, [result])
+  const [isPasswordModalOpen, setIsPasswordModalOpen] = useState(false)
+  const [changePassword, result] = useChangePasswordMutation()
 
-    const passwordFormController = useForm({
-        mode: 'onChange',
-        defaultValues: {
-          currentPassword: '',
-          verifyNewPassword: '',
-          newPassword: ''
-        },
-      })
+  useEffect(() => {
+    if (!result.isSuccess) {
+      toast.error(
+        'Something went wrong when updating your profile'
+      )
+    } else {
+      toast.success('Update successful')
+    }
+  }, [result])
+
+  const passwordFormController = useForm({
+    mode: 'onChange',
+    defaultValues: {
+      currentPassword: '',
+      verifyNewPassword: '',
+      newPassword: ''
+    },
+  })
 
   const handlePasswordSubmit = passwordFormController.handleSubmit((data) => {
-    setIsPasswordModalOpen(false)
-    const formData = new FormData()
-    const request:UpdatePassword = {
-      newPassword:data.newPassword,
-      oldPassword:data.currentPassword,    
-    } 
+    const request: UpdatePassword = {
+      newPassword: data.newPassword,
+      oldPassword: data.currentPassword,
+    }
     changePassword(request)
-    formData.append('newPassword', data.newPassword)
-    formData.append('oldPassword', data.currentPassword)
-    updateMe(formData)
     passwordFormController.reset()
   })
 
   return (
-      <div className='flex flex-col pt-6 space-y-1'>
-        <p className='text-2xl'>Password & Authentification</p>
-        <ModifyPasswordDialog
+    <div className='flex flex-col pt-6 space-y-1'>
+      <p className='text-2xl'>Password & Authentification</p>
+      <ModifyPasswordDialog
         action={handlePasswordSubmit}
         isModalOpen={isPasswordModalOpen}
         setIsModalOpen={setIsPasswordModalOpen}
         passwordFormController={passwordFormController}
       />
-      </div>
+    </div>
   )
-
 }
