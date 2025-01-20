c2f00fd36030f06cf1b66cf64a8a0f1b46ff6e5e
Isalyne <isalyne.llinares-rames@etu.umontpellier.fr>
2025-01-20 18:53:49 +0100

    feat: add webhook entity and export in index

---
+++ libs/shared/contracts/src/lib/entities/webhook.entity.ts
@@ -0,0 +1,10 @@
+export interface WebhookEntity {
+  id: string
+  name: string
+  serverId: string
+  channelId: string
+  profilePicture?: string
+  userId?: string
+  createdAt?: string
+  updatedAt?: string
+}
