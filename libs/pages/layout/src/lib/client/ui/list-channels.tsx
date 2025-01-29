3c7e5421243009656975cc885d0e6c199a5f1435
duratm <duratmathias@gmail.com>
2025-01-29 08:53:06 +0100

    feat(elixir-voice): repare server presence

--- libs/pages/layout/src/lib/client/ui/list-channels.tsx
+++ libs/pages/layout/src/lib/client/ui/list-channels.tsx
@@ -3,6 +3,8 @@ import { Swappable } from '@beep/utils'
 import { useContext, useEffect, useRef } from 'react'
 import { createSwapy, Swapy } from 'swapy'
 import { ChannelContext } from '../feature/channels/channels-navigation-context'
+import { useSelector } from 'react-redux'
+import { getVoiceState } from '@beep/voice'
 import DisplayChannelFeature from '../feature/display-channel-feature'
 import VoiceChannel from '../feature/voice-channel'
 
@@ -10,9 +12,11 @@ export interface ListTextChannelsProps {
   moveChannel: (channelId: string, newPosition: number) => void
 }
 
-export function ListChannels({ moveChannel }: ListTextChannelsProps) {
-  const { streamingUsers: occupiedChannels, channels } =
-    useContext(ChannelContext)
+export function ListChannels({
+  moveChannel,
+}: ListTextChannelsProps) {
+  const { channels} = useContext(ChannelContext)
+  const { serverPresence } = useSelector(getVoiceState)
   const swapy = useRef<Swapy | null>(null)
   const container = useRef(null)
   const startingPosition = useRef<
@@ -49,7 +53,7 @@ export function ListChannels({ moveChannel }: ListTextChannelsProps) {
   return (
     <div ref={container}>
       {channels.map((channel, index) => {
-        const occupiedChannel = occupiedChannels.find((occupiedChannel) => {
+        const occupiedChannel = serverPresence.find((occupiedChannel) => {
           return occupiedChannel.channelId === channel.id
         })
         switch (channel.type) {
