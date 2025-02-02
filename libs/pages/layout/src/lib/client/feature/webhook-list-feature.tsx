f2be2a02cf44dd8f7fef2de1e172836dfe6629b3
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2025-02-02 21:54:04 +0100

    fix warning

--- libs/pages/layout/src/lib/client/feature/webhook-list-feature.tsx
+++ libs/pages/layout/src/lib/client/feature/webhook-list-feature.tsx
@@ -18,31 +18,29 @@ export default function WebhookListFeature({
     isError,
   } = useGetWebHooksServerQuery(serverId)
 
-
   if (isLoading) {
-    return <div>{t('layout.webhook-list.loading')}</div> 
+    return <div>{t('layout.webhook-list.loading')}</div>
   }
 
   if (isError) {
-    return <div>{t('layout.webhook-list.error')}</div> 
+    return <div>{t('layout.webhook-list.error')}</div>
   }
 
   if (!webhooks || webhooks.length === 0) {
-    return <div>{t('layout.webhook-list.no_webhooks')}</div> 
+    return <div>{t('layout.webhook-list.no_webhooks')}</div>
   }
-
 
   if (webhooks.length === 0) {
     return <div>{t('layout.webhook-list.no_webhooks')}</div>
   } else {
     return (
-          <ul>
-            {webhooks!.map((webhook: WebhookEntity) => (
-              <li key={webhook.id}>
-                <WebhookItemFeature webhook={webhook}></WebhookItemFeature>
-              </li>
-            ))}
-          </ul>
+      <ul>
+        {webhooks?.map((webhook: WebhookEntity) => (
+          <li key={webhook.id}>
+            <WebhookItemFeature webhook={webhook}></WebhookItemFeature>
+          </li>
+        ))}
+      </ul>
     )
   }
 }
